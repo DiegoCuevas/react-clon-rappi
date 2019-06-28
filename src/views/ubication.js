228b04cdbb536ebef618045940f38aa4240ef589
paulotijero <paulotijero@gmail.com>
2019-06-28 12:06:58 -0500

    Add ubication url

--- src/views/ubication.js
+++ src/views/ubication.js
@@ -1,17 +1,35 @@
+/** @jsx jsx */
 import React from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import { jsx } from "@emotion/core";
+
 import { useSelectRestaurant } from "../action-hooks";
 import { usePosition } from "../selectors";
 
 const container = {
-  height: "100vh",
-  width: "100vw",
-  margin: "0 auto"
+  height: "80vh"
 };
+
+const markerRest = new L.Icon({
+  iconUrl: require("../assets/marker-restaurant.png"),
+  iconRetinaUrl: require("../assets/marker-restaurant.png"),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [40, 40]
+});
+
+const markerPeople = new L.Icon({
+  iconUrl: require("../assets/marker-people.png"),
+  iconRetinaUrl: require("../assets/marker-people.png"),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [40, 40]
+});
 
 function Ubication({ id = 1 }) {
   const selectRestaurant = useSelectRestaurant();
-  const [position, setPosition] = React.useState([0, 0]);
+  const [position, setPosition] = React.useState([-12.1402803, -76.9881638]);
   const [zoomState, setZoomState] = React.useState(16);
   const positionRest = usePosition();
 
@@ -47,7 +65,7 @@ function Ubication({ id = 1 }) {
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker position={position} icon={markerPeople}>
         <Popup>
           You are here!
           <span role="img" aria-label="emoji dot position">
@@ -55,7 +73,10 @@ function Ubication({ id = 1 }) {
           </span>
         </Popup>
       </Marker>
-      <Marker position={[positionRest.latitud, positionRest.longitud]}>
+      <Marker
+        position={[positionRest.latitud, positionRest.longitud]}
+        icon={markerRest}
+      >
         <Popup>
           This is the restaurant
           <span role="img" aria-label="emoji dot position">
